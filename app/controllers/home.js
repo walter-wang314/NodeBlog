1bd27bfdc66fa6a63a514c51bd300b437d0241af
walter_wally <wzc314159@hotmail.com>
2017-10-06 23:10:19 +0800

    Initial commit

---
+++ app/controllers/home.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  Article = mongoose.model('Article');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+  Article.find(function (err, articles) {
+    if (err) return next(err);
+    res.render('blog/index', {
+      title: 'Node Blog Home',
+      articles: articles,
+      pretty: true
+    });
+  });
+});
+
+router.get('/about', function (req, res, next) {
+    res.render('blog/index', {
+      title: 'About me',
+      pretty: true
+  });
+});
+
+router.get('/contact', function (req, res, next) {
+  res.render('blog/index', {
+    title: 'Contact me',
+    pretty: true
+  });
+});
